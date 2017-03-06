cb4b7f999295a497d33f5f31061ddc623b1dbbe6
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-03-06 14:21:51 -0500

    new:dev: initial project definition

---
+++ default.nix
@@ -0,0 +1,55 @@
+{ pkgs ? import ./nixpkgs.nix
+, venvdir ? ".venv"
+, name ? "cloudmesh_api"
+}:
+
+with pkgs;
+
+let
+
+  inherit (lib)
+    getAttr
+    hasAttr
+    hasPrefix
+    fileContents
+    filter
+    splitString
+    stringLength
+    ;
+
+  readRequirements = file:
+    let
+      cleaner = line: !(hasPrefix "#" line) && (stringLength line > 0);
+      lines = splitString "\n" (fileContents file);
+    in filter cleaner lines;
+
+  findPackages = set: packageNames:
+    let
+      findPkg = name:
+        if hasAttr name set
+        then getAttr name set
+        else throw "Cannot find '${name}'";
+    in map findPkg packageNames;
+
+  findPythonPackages = path: findPackages pythonPackages (readRequirements path);
+
+  requirements      = findPythonPackages ./requirements.open;
+  test_requirements = findPythonPackages ./test_requirements.open;
+
+  python = pythonFull.withPackages (_: requirements ++ test_requirements);
+
+  buildInputs = [
+    cacert
+    libffi
+    openssl
+    pkgconfig
+    sqlite
+    zlib
+  ];
+
+in
+
+pythonPackages.buildPythonPackage {
+  name = "${name}";
+  buildInputs = [ python ] ++ buildInputs;
+}
