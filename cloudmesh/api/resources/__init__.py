b50752a4bfabb1e6e6f858f3c2dac6f76ddb4bfc
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-03-14 10:50:57 -0400

    new:dev: add tests for resources api

--- cloudmesh/api/resources/__init__.py
+++ cloudmesh/api/resources/__init__.py
@@ -46,3 +46,95 @@ class ResourceComposite(Resource):
 
     def deallocate(self):
         self.composite.deallocate()
+
+
+################################################################ TESTING
+
+class _TestResource_TmpDir(Resource):
+
+    @property
+    def data(self):
+        return dict(path=self.path, type='dir')
+
+    def allocate(self):
+        import tempfile
+        self.path = tempfile.mkdtemp(prefix='cloudmesh.resource.test')
+        return self.path
+
+    def deallocate(self):
+        import shutil
+        shutil.rmtree(self.path)
+
+
+class _TestResource_TmpFile(Resource):
+
+    @property
+    def data(self):
+        return dict(path=self.path, type='file', file=self.file)
+
+    def allocate(self):
+        import tempfile
+        self.file, self.path = tempfile.mkstemp()
+        return self.file
+
+
+    def deallocate(self):
+        import os
+        os.close(self.file)
+        os.unlink(self.path)
+
+
+def test_resource_tmpdir():
+
+    import os.path
+
+    r = _TestResource_TmpDir()
+
+    r.allocate()
+
+    path = r.data['path']
+
+    assert os.path.exists(path)
+    assert os.path.isdir(path)
+
+    r.deallocate()
+    assert not os.path.exists(path)
+
+
+def test_resource_tmpfile():
+
+    import os.path
+
+    r = _TestResource_TmpFile()
+
+    r.allocate()
+
+    path = r.data['path']
+
+    assert os.path.exists(path)
+    assert os.path.isfile(path)
+
+    r.deallocate()
+    assert not os.path.exists(path)
+
+
+def test_resource_composite():
+
+    import os.path
+
+    resources = ResourceComposite([
+        _TestResource_TmpDir(),
+        _TestResource_TmpDir(),
+        _TestResource_TmpFile(),
+        _TestResource_TmpFile(),
+    ])
+
+    resources.allocate()
+
+    paths = resources.getattr('path')
+    for p in paths:
+        assert os.path.exists(p)
+
+    resources.deallocate()
+    for p in paths:
+        assert not os.path.exists(p)
