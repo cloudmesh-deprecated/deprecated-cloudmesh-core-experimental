cbbca1f1c31277a3eee1fd042454270d3a3882d5
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-03-14 13:59:46 -0400

    fix:dev: accidentally marked some provider attributes as methods instead of properties

--- cloudmesh/api/provider.py
+++ cloudmesh/api/provider.py
@@ -10,7 +10,7 @@ class Provider(object):
     def name(self):
         "The name of this provider"
 
-    @abstractmethod
+    @abstractproperty
     def nodes(self):
         """List the nodes running
 
@@ -18,7 +18,7 @@ class Provider(object):
         :rtype: :class:`list` of :class:`dict`
         """
 
-    @abstractmethod
+    @abstractproperty
     def images(self):
         """List the images available
 
@@ -26,7 +26,7 @@ class Provider(object):
         :rtype: :class:`list` of :class:`dict`
         """
 
-    @abstractmethod
+    @abstractproperty
     def flavors(self):
         """List the flavors available
 
@@ -34,7 +34,7 @@ class Provider(object):
         :rtype: :class:`list` of :class:`dict`
         """
 
-    @abstractmethod
+    @abstractproperty
     def secgroups(self):
         """List the security groups available
 
@@ -42,7 +42,7 @@ class Provider(object):
         :rtype: :class:`list` of :class:`dict`
         """
 
-    @abstractmethod
+    @abstractproperty
     def addresses(self):
         """List the addresses available
 
