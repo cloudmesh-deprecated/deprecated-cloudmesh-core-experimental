a9e7eb8bceb1a7ce3c8512f21603635778069a7f
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-03-14 10:50:37 -0400

    new:dev: add ResourceComposite class to resources

--- cloudmesh/api/resources/__init__.py
+++ cloudmesh/api/resources/__init__.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
+from cloudmesh.util.patterns import Composite
+
 
 class Resource(object):
     __metaclass__ = ABCMeta
@@ -16,3 +18,31 @@ class Resource(object):
     def deallocate(self):
         "Deallocate this resource"
 
+
+class ResourceComposite(Resource):
+
+    def __init__(self, elements):
+
+        for elem in elements:
+            assert isinstance(elem, Resource)
+
+        self.composite = Composite(elements)
+
+    @property
+    def data(self):
+        return None
+
+    def getattr(self, name):
+        "Return a list of attributes for each element"
+
+        def gen():
+            for elem in self.composite.elements:
+                yield getattr(elem, name)
+
+        return list(gen())
+
+    def allocate(self):
+        self.composite.allocate()
+
+    def deallocate(self):
+        self.composite.deallocate()
