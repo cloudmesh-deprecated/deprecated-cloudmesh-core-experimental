c29842e83263bdd4845a1a779e0cfe8174c46e88
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-03-20 18:03:35 -0400

    chg:dev: openstack: cleanup old keystone auth code

--- cloudmesh/openstack/provider.py
+++ cloudmesh/openstack/provider.py
@@ -11,35 +11,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-AuthResult = namedtuple('AuthResult', ['keystone', 'nova'])
-
-
-def auth_keystone_v2(**authinfo):
-    logger.debug('Using Keystone V2 authentication')
-    from keystoneauth1.identity import v2
-    from keystoneclient.v2_0.client import Client as Keystone
-    from novaclient.client import Client as Nova
-
-    authinfo = Dotdict(authinfo)
-    cacert = authinfo.get('cacert', None)
-    for param in 'username tenant_name auth_url cacert'.split():
-        logger.debug('Authenticating with {}={}'.format(param, authinfo.get(param, None)))
-
-    auth = v2.Password(
-        username = authinfo.username,
-        password = authinfo.password,
-        tenant_name = authinfo.tenant_name,
-        auth_url = authinfo.auth_url,
-    )
-
-    sess = Session(auth=auth)
-    keystone = Keystone(session=sess, cacert=cacert)
-    nova = Nova(2, session=sess)
-
-    return AuthResult(keystone=keystone, nova=nova)
-
-
-
 class Provider(ProviderInterface):
 
     def __init__(self, **kwargs):
